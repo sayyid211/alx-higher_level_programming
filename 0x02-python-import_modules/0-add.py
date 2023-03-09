e09b31eb448c56fc5f91d8d43ed2bf5e7a6704a0
Adamu Muhammad <sayyid211@gmail.com>
2023-03-09 15:06:27 +0000

    add ab

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    from add_0 import add
+    """import addition module"""
+
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
