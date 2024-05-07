543d216e77acb161028de8e2ec54cf0a5e176615
Adamu Muhammad <sayyid211@gmail.com>
2024-05-07 13:13:11 +0000

    update all langs

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,2 @@
 #!/bin/node
-const c = 'C is fun';
-const p = 'Python is cool';
-const j = 'JavaScript is amazing';
-console.log(c);
-console.log(p);
-console.log(j);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
