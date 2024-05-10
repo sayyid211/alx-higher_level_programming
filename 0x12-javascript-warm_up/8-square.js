a54736e2df4deb2aa80ddd8ffccc57f9284629d9
Adamu Muhammad <sayyid211@gmail.com>
2024-05-10 16:03:08 +0000

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2]))) {
+if (isNaN(parseInt(process.argv[2])) || !parseInt(process.argv[2])) {
   console.log('Missing size');
 } else {
   const x = parseInt(process.argv[2]);
