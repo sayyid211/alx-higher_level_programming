3b0fe152ff66515c225bf991a22257a0f958965a
Adamu Muhammad <sayyid211@gmail.com>
2024-05-21 09:47:00 +0000

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-function fact(x) {
-    return isNaN(x) || x === 0 ? 1 : x * fact(x - 1);
+function fact (x) {
+  return isNaN(x) || x === 0 ? 1 : x * fact(x - 1);
 }
 console.log(fact(Number(process.argv[2])));
